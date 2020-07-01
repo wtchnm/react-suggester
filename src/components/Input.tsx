ab0555ccf97afb84a974a3b8799e5ab13f95c4bc
Gabriel Pereira Woitechen <gabriel.woitechen@medcloud.co>
2020-07-01 08:16:13 -0300

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+function Input(): ReactElement {
+  return <input />;
+}
+
+export default Input;
---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Input from "../components/Input";
+
+export default {
+  title: "Input",
+  component: Input,
+};
+
+export function Default() {
+  return <Input />;
+}
