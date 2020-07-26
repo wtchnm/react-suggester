b23da691156c00eb5f8be16e5592f422eb373d14
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2020-07-26 13:18:20 -0300

    perf(input): remove unnecessary label null check

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,43 +9,43 @@ interface Props {
   placeholder?: string;
 
   onFocus?(event: React.FocusEvent<HTMLInputElement>): void;
-  onMouseDown?(event: React.MouseEvent<HTMLInputElement, MouseEvent>): void;
   onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
+  onMouseDown?(event: React.MouseEvent<HTMLInputElement, MouseEvent>): void;
   onKeyDown?(event: React.KeyboardEvent<HTMLInputElement>): void;
 }
 
 function Input({
   value,
   onChange,
+
   label,
   placeholder,
+
   onFocus,
-  onMouseDown,
   onBlur,
+  onMouseDown,
   onKeyDown,
 }: Props): ReactElement {
   const componentId = useMemo(() => `Input-${nanoid()}`, []);
 
   return (
     <div className="flex flex-col">
-      {label && (
-        <label
-          className="text-gray-700 mb-1 pr-4 leading-normal"
-          htmlFor={componentId}
-        >
-          {label}
-        </label>
-      )}
+      <label
+        htmlFor={componentId}
+        className="text-gray-700 mb-1 pr-4 leading-normal"
+      >
+        {label}
+      </label>
       <input
+        type="text"
+        id={componentId}
         value={value}
         onChange={onChange}
-        className="appearance-none bg-white border border-solid border-gray-400 rounded w-full py-2 px-3 focus:outline-none focus:shadow-outline focus:border-blue-400 leading-normal box-border text-base placeholder-gray-500"
-        id={componentId}
-        type="text"
         placeholder={placeholder}
+        className="appearance-none bg-white border border-solid border-gray-400 rounded w-full py-2 px-3 focus:outline-none focus:shadow-outline focus:border-blue-400 leading-normal box-border text-base placeholder-gray-500"
         onFocus={onFocus}
-        onMouseDown={onMouseDown}
         onBlur={onBlur}
+        onMouseDown={onMouseDown}
         onKeyDown={onKeyDown}
       />
     </div>
