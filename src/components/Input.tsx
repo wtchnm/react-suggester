2b0bdee6ae049faf2fb9d3710340cb104420747b
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2020-08-05 21:35:29 -0300

    feat: pass any standard attribute to the input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,30 +1,14 @@
 import React, { useMemo, ReactElement } from "react";
 import { nanoid } from "nanoid/non-secure";
 
-interface Props {
-  value?: string | number;
-  onChange?(event: React.ChangeEvent<HTMLInputElement>): void;
-
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  placeholder?: string;
-
-  onFocus?(event: React.FocusEvent<HTMLInputElement>): void;
-  onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
-  onMouseDown?(event: React.MouseEvent<HTMLInputElement, MouseEvent>): void;
-  onKeyDown?(event: React.KeyboardEvent<HTMLInputElement>): void;
 }
 
 function Input({
-  value,
-  onChange,
-
   label,
-  placeholder,
 
-  onFocus,
-  onBlur,
-  onMouseDown,
-  onKeyDown,
+  ...rest
 }: Props): ReactElement {
   const componentId = useMemo(() => `Input-${nanoid()}`, []);
 
@@ -36,14 +20,9 @@ function Input({
       <input
         type="text"
         id={componentId}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
         className="mt-1 box-border w-full py-2 px-3 rounded border border-solid border-gray-400 bg-white text-base leading-normal placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-blue-400"
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onMouseDown={onMouseDown}
-        onKeyDown={onKeyDown}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...rest}
       />
     </div>
   );
