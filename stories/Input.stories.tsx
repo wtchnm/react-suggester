4da9cb9961fa4885c05715ee83e4fa6fd6994b71
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2020-08-05 21:36:27 -0300

    chore(storybook): add controlled input story

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useCallback } from "react";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import Input from "../src/components/Input";
 
@@ -14,6 +14,27 @@ export function Default(): ReactElement {
   return (
     <div className="p-4 max-w-md box-border">
       <Input label={label} placeholder={placeholder} />
+    </div>
+  );
+}
+
+export function Controlled(): ReactElement {
+  const label = text("Label", "Name");
+  const placeholder = text("Placeholder", "John Doe");
+  const [value, setValue] = useState("");
+
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }, []);
+
+  return (
+    <div className="p-4 max-w-md box-border">
+      <Input
+        label={label}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
