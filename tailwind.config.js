5f83cea0240f446adecd0c28a83a966138a8cc12
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2020-08-20 14:33:29 -0300

    refactor: feat: opt-in to future-facing breaking changes on tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   purge: ["./src/**/*.tsx", "./stories/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "active"],
   },
-  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
 };
