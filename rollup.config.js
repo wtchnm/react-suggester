18a361c474bfd9a84bf1562b58b2a77038d46b73
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2020-08-26 09:11:06 -0300

    Revert "build: replace rollup-plugin-size-snapshot by rollup-plugin-size"
    This reverts commit dfe692007798112d522ef9194ab93484971077f5.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { babel } from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import size from "rollup-plugin-size";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 
 const extensions = [".js", ".jsx", ".es6", ".es", ".mjs", ".ts", ".tsx"];
 
@@ -13,7 +13,6 @@ const common = {
     react: "React",
   },
   exports: "named",
-  plugins: [size()],
 };
 
 export default {
@@ -50,6 +49,7 @@ export default {
       extract: "ReactSuggester.css",
       sourceMap: true,
     }),
+    sizeSnapshot(),
     terser(),
   ],
 };
