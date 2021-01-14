5af5ae22fc77e25ee8578ddeca16dd519062de2c
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2021-01-14 09:54:30 -0300

    chore: upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   purge: ["./src/**/*.tsx"],
   variants: {
-    backgroundColor: ["responsive", "hover", "focus", "active"],
-  },
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true,
+    extend: {
+      backgroundColor: ["active"],
+    },
   },
 };
